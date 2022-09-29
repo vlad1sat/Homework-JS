76afb48f93e028794f415b6b968040d8a7fbe8a8
vlad1sat <pyatkov200316@mail.ru>
2022-09-29 10:50:40 +0500

    update object-array-filter.js

--- 6.7/object-array-filter.js
+++ 6.7/object-array-filter.js
@@ -1,9 +1,8 @@
 function filter(objects, property, valueProperty) {
     let filteredObject = []
     for (let object of objects) {
-        if (object.hasOwnProperty(property)) {
-            if (object[property] === valueProperty)
-                filteredObject.push(object)
+        if (object.hasOwnProperty(property) && object[property] === valueProperty) {
+            filteredObject.push(object)
         }
     }
     if (filteredObject.length === 0)
