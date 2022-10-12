3181e34c3531b0bd5fc744a33d465af745472503
vlad1sat <pyatkov200316@mail.ru>
2022-10-12 22:29:14 +0500

    add 7.6

---
+++ 7.6/task_2/main.js
@@ -0,0 +1,15 @@
+function changeTextTimeOut() {
+    setTimeout(() => {
+        changeText()
+    }, 300)
+}
+
+function changeText() {
+    h2.textContent = input_user.value
+}
+
+let input_user = document.createElement('input')
+let h2 = document.createElement('h2')
+document.body.append(input_user)
+document.body.append(h2)
+input_user.addEventListener('input', changeTextTimeOut)
