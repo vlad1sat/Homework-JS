589b3ce60d9b2b5867dc9200f4468cf78538ddc7
vlad1sat <pyatkov200316@mail.ru>
2022-10-16 12:47:32 +0500

    modified task 7

--- 7.6/task_1/main.js
+++ 7.6/task_1/main.js
@@ -1,25 +1,36 @@
 (function () {
-    let input_user = document.createElement('input')
-    let button = document.createElement('button')
-    let div_element = document.createElement('div')
-    document.body.append(input_user)
-    document.body.append(button)
-    document.body.append(div_element)
-    div_element.textContent = "0"
-    button.textContent = "Старт таймера"
-    let timer
+    let input_user = addInApp('input');
+    let button = addInApp('button');
+    let div_element = addInApp('div');
+    div_element.textContent = "0";
+    button.textContent = "Старт таймера";
+    let timer;
 
     function onClick() {
-        let value_timer = parseInt(input_user.value)
-        clearInterval(timer)
+        let value_timer = parseInt(input_user.value);
+        clearInterval(timer);
         timer = setInterval(() => {
-            console.log(value_timer)
-            div_element.textContent = value_timer.toString()
+            console.log(value_timer);
+            div_element.textContent = value_timer;
             if (value_timer <= 0)
-                clearInterval(timer)
-            value_timer--
+                clearInterval(timer);
+            value_timer--;
         }, 1000)
     }
 
-    document.addEventListener('click', onClick)
+    function addInApp (element) {
+        let elementApp = document.createElement(element);
+        document.body.append(elementApp);
+        return elementApp;
+    }
+
+    function valueInput() {
+        if (input_user.value === '')
+            div_element.textContent = '0';
+        div_element.textContent = input_user.value;
+    }
+
+    input_user.addEventListener('input', valueInput);
+
+    button.addEventListener('click', onClick);
 })();
--- 7.6/task_2/main.js
+++ 7.6/task_2/main.js
@@ -1,18 +1,17 @@
 (function () {
-
     function changeTextTimeOut() {
         setTimeout(() => {
-            changeText()
-        }, 300)
+            changeText();
+        }, 300);
     }
 
     function changeText() {
-        h2.textContent = input_user.value
+        h2.textContent = input_user.value;
     }
 
-    let input_user = document.createElement('input')
-    let h2 = document.createElement('h2')
-    document.body.append(input_user)
-    document.body.append(h2)
-    input_user.addEventListener('input', changeTextTimeOut)
+    let input_user = document.createElement('input');
+    let h2 = document.createElement('h2');
+    document.body.append(input_user);
+    document.body.append(h2);
+    input_user.addEventListener('input', changeTextTimeOut);
 })();
